50739382e00b0ae2bac2531d5232692a5d728d92
Rohan <rohan17.rp@gmail.com>
2020-10-27 16:19:45 +0530

    Bencoding and getting peers list from tracker

---
+++ bencode_torrent.py
@@ -0,0 +1,42 @@
+import bencodepy
+import sys
+
+class torrent:
+	def __init__(self, torrent_file):
+		'''
+		Initialise various variables for a torrent.
+		torrent_file must be path to .torrent file
+		Currently this handles only single file case.
+		'''
+		
+		self.file_extract = bencodepy.bread(torrent_file) 
+		self.tracker = []
+		if b'announce-list' in self.file_extract:
+		    for i in self.file_extract[b'announce-list']:
+		        self.tracker.append(i[0].decode())
+		else:
+		    self.tracker = [self.file_extract[b'announce'].decode()]
+		if b'encoding' in self.file_extract:
+		    self.encoding = self.file_extract[b'encoding'].decode()
+		else:
+		    self.encoding = 'UTF-8'
+		self.piece_len = self.file_extract[b'info'][b'piece length']
+		self.piece = self.file_extract[b'info'][b'pieces']
+		self.name = self.file_extract[b'info'][b'name'].decode()
+		self.length = self.file_extract[b'info'][b'length']
+	
+	def print_file_info(self):
+		print("Trackers :-")
+		for i in self.tracker:
+			print("\t" + i)
+		print("Piece length :- " + str(self.piece_len))
+		print("Hash :- " + str(self.piece.hex()))
+		print("Name :- " + self.name)
+		print("Length :- " + str(self.length))
+
+if __name__ == '__main__':
+	if len(sys.argv) > 1:
+		file_name = torrent(sys.argv[1])
+		file_name.print_file_info()
+	else:
+		print("Insufficient arguments")
---
+++ tracker.py
@@ -0,0 +1,50 @@
+import bencode_torrent
+import requests
+import hashlib
+import sys
+import os
+from datetime import datetime
+import bencodepy
+from struct import *
+
+class tracker:
+	def __init__(self, url, torrent):
+		self.url = url
+		info_hash = hashlib.sha1()
+		peer_id = hashlib.sha1()
+		peer_id.update(str(os.getpid()).encode())
+		peer_id.update(str(datetime.now()).encode())
+		info_bencode = torrent.file_extract[b'info']
+		info_hash.update(bencodepy.encode(info_bencode))
+		self.payload = {
+			'info_hash': info_hash.digest(),
+			'peer_id' : peer_id.digest(),
+			'port' : 3128,
+			'uploaded' : 0,
+			'downloaded' : 0,
+			'left' : torrent.length,
+			'compact' : 1,
+			'event' : 'started',
+#                'ip' : '10.1.101.150'
+		}
+		self.tracker_respond = requests.get(url, params = self.payload)
+		self.tracker_respond = bencodepy.decode(self.tracker_respond.content)
+		self.peers = self.tracker_respond[b'peers']
+		self.peers_list = []
+		for i in range(int(len(self.peers) / 6)):
+			self.peers_list.append([str(self.peers[6 * i]) + '.' +
+									str(self.peers[6 * i + 1]) + '.' +
+									str(self.peers[6 * i + 2]) + '.' +
+									str(self.peers[6 * i + 3]),
+									self.peers[6 * i + 4] * (2 ** 8) +
+									self.peers[6 * i + 5]])
+
+
+if __name__ == '__main__':
+	if len(sys.argv) > 1:
+		file_name = sys.argv[1]
+		new_file = bencode_torrent.torrent(file_name)
+		t = tracker(new_file.tracker[0], new_file)
+		print(t.peers_list)
+	else:
+		print("Insufficient arguments")
